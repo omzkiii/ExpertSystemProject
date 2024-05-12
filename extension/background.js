f7f3636a9f19b3b5793aef6d1b455663e3ee78ea
joshua jabat <joshuajabat@gmail.com>
2024-05-12 09:07:12 +0800

    init

---
+++ extension/background.js
@@ -0,0 +1,52 @@
+chrome.tabs.onActivated.addListener(function (activeInfo) {
+    chrome.tabs.get(activeInfo.tabId, function (tab) {
+        var currentTime = new Date().toLocaleTimeString();
+        console.log(`[${currentTime}] Activated tab:`, tab.url);
+
+        // Set a timeout to execute the event after 3 minutes
+        setTimeout(function() {
+            var currentTime = new Date().toLocaleTimeString();
+            console.log(`[${currentTime}] Event triggered after 3 minutes.`);
+            // Execute your event here
+        }, 180000); // 3 minutes in milliseconds
+    });
+});
+
+chrome.tabs.onUpdated.addListener((tabId, change, tab) => {
+    if (tab.active && change.url) {
+        var currentTime = new Date().toLocaleTimeString();
+        console.log(`[${currentTime}] Updated tab:`, change.url);
+
+        // Set a timeout to execute the event after 3 minutes
+        setTimeout(function() {
+            var currentTime = new Date().toLocaleTimeString();
+            console.log(`[${currentTime}] Event triggered after 3 minutes.`);
+            // Execute your event here
+        }, 180000); // 3 minutes in milliseconds
+    }
+});
+
+var tabToUrl = {};
+
+chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
+    tabToUrl[tabId] = tab.url;
+});
+
+chrome.tabs.onRemoved.addListener(function (tabId, removeInfo) {
+    var currentTime = new Date().toLocaleTimeString();
+    console.log(`[${currentTime}] Removed tab:`, tabToUrl[tabId]);
+
+    delete tabToUrl[tabId];
+});
+
+chrome.tabs.onCreated.addListener(function (tab) {
+    var currentTime = new Date().toLocaleTimeString();
+    console.log(`[${currentTime}] Tab created:`, tab.url);
+
+    // Set a timeout to execute the event after 3 minutes
+    setTimeout(function() {
+        var currentTime = new Date().toLocaleTimeString();
+        console.log(`[${currentTime}] Event triggered after 3 minutes for tab:`, tab.url);
+        // Execute your event here
+    }, 3000); // 3 minutes in milliseconds
+});
